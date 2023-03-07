efa321fd1b05abb20c01ac82e13e9c3f8ffe69a4
Sean Hess <seanhess@gmail.com>
2023-03-07 13:37:48 -0700

    Added logo bar

--- client/comp/Layout.tsx
+++ client/comp/Layout.tsx
@@ -7,9 +7,12 @@ interface Props {
 export const Layout:FC<Props> =({children}) =>{
   return(
     <>
-      <div className="min-h-screen flex flex-row justify-center">
-        <div className="max-w-4xl grow sm:flex flex-row">
-          {children}
+      <div className="min-h-screen flex flex-col">
+        <div className="h-24 p-3 px-8"><Logo/></div>
+        <div className="flex flex-row justify-center">
+          <div className="max-w-4xl grow sm:flex flex-row">
+            {children}
+          </div>
         </div>
       </div>
     </>
@@ -20,7 +23,7 @@ export const Layout:FC<Props> =({children}) =>{
 export const Content:FC<Props> =({children}) =>{
   return(
     <>
-      <div className="grow flex flex-col p-4 g-4">
+      <div className="grow flex flex-col p-4 gap-4">
         {children}
       </div>
     </>
@@ -34,5 +37,14 @@ export const Sidebar:FC<Props> =({children}) =>{
         {children}
       </div>
     </>
+  )
+}
+
+
+export const Logo = () => {
+  return (
+    <a id="logo" className="" href="https://satellitedisplay.com" target="_blank">
+      <img className="h-full" alt="Badger by Satellite Display" src="https://satellitedisplay.com/wp-content/uploads/2020/05/Logo-with-Badger-1.png"></img>
+    </a>
   )
 }
